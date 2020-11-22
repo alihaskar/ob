41d96a00cce597758389e34fef611b06a7a5bdbd
Stephen Henry <stephenry@gmail.com>
2020-11-22 21:04:50 +0000

    Ancillary project utilities.

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((fill-column . 80)))
+ (verilog-mode . ((verilog-indent-level . 2)
+                  (verilog-indent-level-behavioral . 2)
+                  (verilog-indent-level-declaration . 2)
+                  (verilog-indent-level-module . 2)
+                  (verilog-auto-lineup . all))))
